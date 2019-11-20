5d9ab3bd59a3b1310c6a2ea7f776c12d05c45d90
nathanspinetti <42946584+nathanspinetti@users.noreply.github.com>
2019-11-20 01:11:41 -0500

    Completion
    Completed the website with a ton of minor adjustments. Added more parallaxing with fruits. Made it responsive the best I could, mobile isn't the best, tablet looks great.

--- js/main.js
+++ js/main.js
@@ -18,3 +18,29 @@ $('.startbutton').click(function(){
 	tl.play();
 })
 document.querySelector("body > section.background1 > div > a > h1")
+
+var tl1 = new TimelineMax({paused:true});
+
+tl1.from( $('.q1'), 2, { opacity: 0, ease: "back.out(1.7)" } ),
+tl1.from( $('.a1'), 2, { opacity: 0, ease: "back.out(1.7)" } ),
+tl1.to( $('.q1, .a1'), 4, { delay:4, opacity: 0, ease: "back.out(1.7)" } ),
+tl1.from( $('.q2'), 2, { opacity: 0, ease: "back.out(1.7)" } ),
+tl1.from( $('.a2'), 2, { opacity: 0, ease: "back.out(1.7)" } ),
+tl1.to( $('.q2, .a2'), 4, { delay:4, opacity: 0, ease: "back.out(1.7)" } ),
+tl1.from( $('.q3'), 2, { opacity: 0, ease: "back.out(1.7)" } ),
+tl1.from( $('.a3'), 2, { opacity: 0, ease: "back.out(1.7)" } ),
+tl1.to( $('.q3, .a3'), 4, { delay:4, opacity: 0, ease: "back.out(1.7)" } ),
+tl1.from( $('.q4'), 2, { opacity: 0, ease: "back.out(1.7)" } ),
+tl1.from( $('.a4'), 2, { opacity: 0, ease: "back.out(1.7)" } ),
+tl1.to( $('.q4, .a4'), 4, { delay:4, opacity: 0, ease: "back.out(1.7)" } ),
+tl1.from( $('.q5'), 2, { opacity: 0, ease: "back.out(1.7)" } ),
+tl1.from( $('.a5'), 2, { opacity: 0, ease: "back.out(1.7)" } ),
+tl1.to( $('.q5, .a5'), 4, { delay:4, opacity: 0, ease: "back.out(1.7)" } ),
+tl1.from( $('.q6'), 2, { opacity: 0, ease: "back.out(1.7)" } ),
+tl1.from( $('.a6'), 2, { opacity: 0, ease: "back.out(1.7)" } ),
+tl1.to( $('.q6, .a6'), 4, { delay:4, opacity: 0, ease: "back.out(1.7)" } );
+
+$('.qbutton').click(function(){
+	tl1.play();
+})
+document.querySelector("body > div.bg8 > section > a > h1")
